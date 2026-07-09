b24abd8c447dbb504f2d021bce540796c12fdb1b
rauquehernan196 <rauquehernan196@gmail.com>
2026-07-08 20:44:31 -0400

    Arreglo API.js

--- vite-project/src/services/api.js
+++ vite-project/src/services/api.js
@@ -1,16 +1,14 @@
+const API_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Chile";
 
-
-export const getChileanMeals = () => {
-    const miUrlApi = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Chile";
-
-
-    return fetch(miUrlApi)
-        .then(response => {
- 
-            if (!response.ok) {
-                throw new Error(`Error en la petición: ${response.status}`);
-            }
-            
-            return response.json();
-        });
+export const getMenuChileno = async () => {
+  try {
+    const res = await fetch(API_URL);
+    if (!res.ok) throw new Error("Error en la red");
+    
+    const { meals } = await res.json();
+    return meals || [];
+  } catch (err) {
+    console.error("Fallo al conectar con la API:", err);
+    throw err;
+  }
 };
